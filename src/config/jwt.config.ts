2857645a0123ec3a056b48cefc772cb45ab3d983
Hans Tjandra <hans.tjandra@komunal.co.id>
2024-08-22 21:46:45 +0700

    feat: jwt init finish'

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,5 +1,5 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('jwt', () => ({
-  secret: process.env.JWT_SECRET || 'super-secret-key',
+  secret: process.env.JWT_ADMIN_KEY || 'super-secret-key',
 }));

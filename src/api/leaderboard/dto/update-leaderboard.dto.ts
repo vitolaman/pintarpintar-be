ac52c1f63a679684ced278bc679f65d2f2f57068
Hans Tjandra <hans.tjandra@komunal.co.id>
2024-08-27 22:01:35 +0700

    feat: leaderboard feature

--- src/api/leaderboard/dto/update-leaderboard.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateLeaderboardDto } from './create-leaderboard.dto';
-
-export class UpdateLeaderboardDto extends PartialType(CreateLeaderboardDto) {}
--- src/api/leaderboard/leaderboard.service.ts
+++ src/api/leaderboard/leaderboard.service.ts
@@ -1,26 +1,179 @@
-import { Injectable } from '@nestjs/common';
-import { CreateLeaderboardDto } from './dto/create-leaderboard.dto';
-import { UpdateLeaderboardDto } from './dto/update-leaderboard.dto';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Response } from 'express';
+import { MonthlyReferralLeaderboard } from './entities/monthly-referral-leaderboard.entity';
+import { Repository } from 'typeorm';
+import { WeeklyPredictionLeaderboard } from './entities/weekly-prediction-leaderboard.entity';
+import { YearlyLeaderboard } from './entities/yearly-prediction-leaderboard.entity';
 
 @Injectable()
 export class LeaderboardService {
-  create(createLeaderboardDto: CreateLeaderboardDto) {
-    return 'This action adds a new leaderboard';
+  constructor(
+    @InjectRepository(WeeklyPredictionLeaderboard)
+    private readonly weeklyPredictionLeaderboardRepository: Repository<WeeklyPredictionLeaderboard>,
+    @InjectRepository(MonthlyReferralLeaderboard)
+    private readonly monthlyReferralLeaderboardRepository: Repository<MonthlyReferralLeaderboard>,
+    @InjectRepository(YearlyLeaderboard)
+    private readonly yearlyLeaderboardRepository: Repository<YearlyLeaderboard>,
+  ) {}
+
+  async weeklyLeaderboard(userId: string, res: Response) {
+    const topUsers = await this.weeklyPredictionLeaderboardRepository
+      .createQueryBuilder('leaderboard')
+      .leftJoinAndSelect('users', 'user', 'leaderboard.userId = user.id')
+      .select([
+        'leaderboard.userId as "userId"',
+        'leaderboard.sumPoint as "sumPoint"',
+        'leaderboard.updated_at',
+        'user.username',
+        'RANK() OVER (ORDER BY leaderboard.sumPoint DESC, leaderboard.updated_at ASC) AS rank',
+      ])
+      .orderBy('leaderboard.sumPoint', 'DESC')
+      .addOrderBy('leaderboard.updated_at', 'ASC')
+      .limit(20)
+      .getRawMany();
+
+    const loggedInUser = await this.weeklyPredictionLeaderboardRepository
+      .createQueryBuilder('leaderboard')
+      .leftJoinAndSelect('users', 'user', 'leaderboard.userId = user.id')
+      .select([
+        'leaderboard.user_id as "userId"',
+        'leaderboard.sum_point as "sumPoint"',
+        'leaderboard.updated_at',
+        'user.username',
+        'RANK() OVER (ORDER BY leaderboard.sumPoint DESC, leaderboard.updated_at ASC) AS rank',
+      ])
+      .where('leaderboard.userId = :userId', { userId })
+      .getRawOne();
+
+    return res.status(HttpStatus.OK).json({
+      responseMessage: `Get Top Weekly Leaderboard Success`,
+      data: {
+        topUsers:
+          topUsers.map((user) => ({
+            rank: user.rank,
+            userId: user.userId,
+            username: user.user_username,
+            sumPoint: user.sumPoint,
+            updated_at: user.leaderboard_updated_at,
+          })) || [],
+        loggedInUser: loggedInUser
+          ? {
+              rank: loggedInUser.rank,
+              userId: loggedInUser.userId,
+              username: loggedInUser.user_username,
+              sumPoint: loggedInUser.sumPoint,
+              updated_at: loggedInUser.leaderboard_updated_at,
+            }
+          : null,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all leaderboard`;
+  async monthlyLeaderboard(userId: string, res: Response) {
+    const topUsers = await this.monthlyReferralLeaderboardRepository
+      .createQueryBuilder('leaderboard')
+      .leftJoinAndSelect('users', 'user', 'leaderboard.userId = user.id')
+      .select([
+        'leaderboard.userId as "userId"',
+        'leaderboard.sumPoint as "sumPoint"',
+        'leaderboard.updated_at',
+        'user.username',
+        'RANK() OVER (ORDER BY leaderboard.sumPoint DESC, leaderboard.updated_at ASC) AS rank',
+      ])
+      .orderBy('leaderboard.sumPoint', 'DESC')
+      .addOrderBy('leaderboard.updated_at', 'ASC')
+      .limit(20)
+      .getRawMany();
+
+    const loggedInUser = await this.monthlyReferralLeaderboardRepository
+      .createQueryBuilder('leaderboard')
+      .leftJoinAndSelect('users', 'user', 'leaderboard.userId = user.id')
+      .select([
+        'leaderboard.user_id as "userId"',
+        'leaderboard.sum_point as "sumPoint"',
+        'leaderboard.updated_at',
+        'user.username',
+        'RANK() OVER (ORDER BY leaderboard.sumPoint DESC, leaderboard.updated_at ASC) AS rank',
+      ])
+      .where('leaderboard.userId = :userId', { userId })
+      .getRawOne();
+
+    return res.status(HttpStatus.OK).json({
+      responseMessage: `Get Top Monthly Leaderboard Success`,
+      data: {
+        topUsers:
+          topUsers.map((user) => ({
+            rank: user.rank,
+            userId: user.userId,
+            username: user.user_username,
+            sumPoint: user.sumPoint,
+            updated_at: user.leaderboard_updated_at,
+          })) || [],
+        loggedInUser: loggedInUser
+          ? {
+              rank: loggedInUser.rank,
+              userId: loggedInUser.userId,
+              username: loggedInUser.user_username,
+              sumPoint: loggedInUser.sumPoint,
+              updated_at: loggedInUser.leaderboard_updated_at,
+            }
+          : null,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} leaderboard`;
-  }
+  async yearlyLeaderboard(userId: string, res: Response) {
+    const topUsers = await this.yearlyLeaderboardRepository
+      .createQueryBuilder('leaderboard')
+      .leftJoinAndSelect('users', 'user', 'leaderboard.userId = user.id')
+      .select([
+        'leaderboard.userId as "userId"',
+        'SUM(leaderboard.sumPoint) as "sumPoint"',
+        'MAX(leaderboard.updated_at) as "updated_at"',
+        'user.username',
+        'RANK() OVER (ORDER BY SUM(leaderboard.sumPoint) DESC, MAX(leaderboard.updated_at) ASC) AS rank',
+      ])
+      .groupBy('leaderboard.userId, user.username')
+      .orderBy('SUM(leaderboard.sumPoint)', 'DESC')
+      .addOrderBy('MAX(leaderboard.updated_at)', 'ASC')
+      .limit(20)
+      .getRawMany();
 
-  update(id: number, updateLeaderboardDto: UpdateLeaderboardDto) {
-    return `This action updates a #${id} leaderboard`;
-  }
+    const loggedInUser = await this.yearlyLeaderboardRepository
+      .createQueryBuilder('leaderboard')
+      .leftJoinAndSelect('users', 'user', 'leaderboard.userId = user.id')
+      .select([
+        'leaderboard.userId as "userId"',
+        'SUM(leaderboard.sumPoint) as "sumPoint"',
+        'MAX(leaderboard.updated_at) as "updated_at"',
+        'user.username',
+        'RANK() OVER (ORDER BY SUM(leaderboard.sumPoint) DESC, MAX(leaderboard.updated_at) ASC) AS rank',
+      ])
+      .where('leaderboard.userId = :userId', { userId })
+      .groupBy('leaderboard.userId, user.username')
+      .getRawOne();
 
-  remove(id: number) {
-    return `This action removes a #${id} leaderboard`;
+    return res.status(HttpStatus.OK).json({
+      responseMessage: `Get Top Yearly Leaderboard Success`,
+      data: {
+        topUsers: topUsers.map((user) => ({
+          rank: user.rank,
+          userId: user.userId,
+          username: user.username,
+          sumPoint: parseInt(user.sumPoint, 10),
+          updated_at: user.updated_at,
+        })),
+        loggedInUser: loggedInUser
+          ? {
+              rank: loggedInUser.rank,
+              userId: loggedInUser.userId,
+              username: loggedInUser.username,
+              sumPoint: parseInt(loggedInUser.sumPoint, 10),
+              updated_at: loggedInUser.updated_at,
+            }
+          : null,
+      },
+    });
   }
 }
